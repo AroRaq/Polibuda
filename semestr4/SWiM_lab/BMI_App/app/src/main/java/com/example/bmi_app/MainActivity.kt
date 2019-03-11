2f6825991206919d161a376f481278764aba15a1
Arkadiusz Rasz <arkadiusz.rasz@gmail.com>
2019-03-11 22:17:30 +0100

    options menu

--- semestr4/SWiM_lab/BMI_App/app/src/main/java/com/example/bmi_app/MainActivity.kt
+++ semestr4/SWiM_lab/BMI_App/app/src/main/java/com/example/bmi_app/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.bmi_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import com.example.bmi_app.logic.BMI_kgcm
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,12 +14,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    /*
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.mainmenu, menu)
+        menuInflater.inflate(R.menu.optionsmenu, menu)
         return true
     }
-    */
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.Settings -> {
+                OpenSettings()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun OpenSettings() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 
     public fun onCalculateClick(view: View) {
         var weight = weightInput.text.toString().toDouble()
