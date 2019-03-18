b8c0547267308b33ea12bab704f26685a8558e24
Arkadiusz Rasz <arkadiusz.rasz@gmail.com>
2019-03-18 15:27:35 +0100

    BMI_pp +MSiD lab 1

--- semestr4/MSiD_lab/wstep/hello.py
+++ semestr4/MSiD_lab/wstep/hello.py
@@ -11,12 +11,34 @@ import matplotlib.pyplot as plt
 #czy liczba jest 2^n
 #lista elementów, zwrócić listę 192837465
 
-def shorter(str, len): 
+#%%
+def shorter(str, length): 
     words = str.split(' ')
     ret = ""
     for word in words: 
-        if (1 < len):
+        if (len(word) < length):
             ret += word + ' '
-    return ret
+    return ret.strip()
 
-shorter("1234 123456 123 1 123456789", 6)
+shorter("4444 666666 333 1 999999999", 6)
+
+#%%
+import math
+def isPowerOf2(number):
+    return math.log2(number) == math.floor(log2(number))
+    
+#%%
+isPowerOf2(15)
+#%%
+isPowerOf2(1)
+#%%
+isPowerOf2(128)
+
+#%%
+def weirdList(src):
+    if len(src) > 0:
+        return [src[0]] + weirdList(src[1::][::-1])
+    else:
+        return []
+
+weirdList([1,2,3,4,5,6,7,8,9])
--- semestr4/SWiM_lab/BMI_App/app/src/main/java/com/example/bmi_app/MainActivity.kt
+++ semestr4/SWiM_lab/BMI_App/app/src/main/java/com/example/bmi_app/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.bmi_app
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import com.example.bmi_app.logic.BMI_kgcm
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -23,23 +26,38 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         return when (item?.itemId) {
             R.id.Settings -> {
-                OpenSettings()
+                openSettings()
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
     }
 
-    private fun OpenSettings() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    private fun openSettings() {
+        val intent = Intent(this, SettingsActivity::class.java)
+        startActivity(intent)
     }
 
 
     public fun onCalculateClick(view: View) {
-        var weight = weightInput.text.toString().toDouble()
-        var height = heightInput.text.toString().toDouble()
-        var calculator = BMI_kgcm(weight, height)
-        var bmi = calculator.calcBMI()
-        bmiText.text = bmi.toString()
+        val weight = weightInput.text.toString().toDouble()
+        val height = heightInput.text.toString().toDouble()
+        val calculator = BMI_kgcm(weight, height)
+        try {
+            val bmi = calculator.calcBMI()
+            bmiRating.text = bmi.toString()
+            val (message, color) = when {
+                bmi < 18.5 -> Pair("Underweight", R.color.darkblue)
+                bmi < 25 -> Pair("Normal", R.color.lapislazuli)
+                bmi < 30 -> Pair("Overweight", R.color.grynszpan)
+                bmi < 35 -> Pair("Obese", R.color.orange)
+                else -> Pair("Extremely Obese", R.color.rozpompejanski)
+            }
+            bmiText.text = message
+            bmiText.setTextColor(color)
+        }
+        catch (e: IllegalArgumentException) {
+            Toast.makeText(this, "Wrong arguments", Toast.LENGTH_LONG).show()
+        }
     }
 }
